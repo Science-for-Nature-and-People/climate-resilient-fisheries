bdd3352d61a5c49e81503c4956f102d35f7d6e5a
megfletch <mjfletcher@ucsb.edu>
2022-06-07 12:26:00 -0700

    uploaded current raw data as of 6/7 and created new clean data file for case_synth_learning paper

--- case_synth_learning/01-load-data.R
+++
@@ -1,7 +0,0 @@
-# 01-load-data.R
-# Example script for getting started
-library(here) 
-source(here("case_study_analysis","00-load-libraries.R")) 
-
-load(here("case_study_analysis", "clean_data","attribute_table_clean.Rdata"))
-case_summaries <- read_csv(here("case_study_analysis","clean_data","case_summaries.csv"))
