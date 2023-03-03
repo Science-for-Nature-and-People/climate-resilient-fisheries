625616871d9cd130ee2041f03dd63e6ae53ae89f
lilyzzhao <lilyzhao@ucsb.edu>
2023-03-03 11:06:19 -0800

    sup_fig addition

--- significance testing of attributes.R
+++ significance testing of attributes.R
@@ -14,8 +14,10 @@ plotdir <- "case_study_analysis/lz_scripts"
 data_orig <- readRDS(file.path(datadir, "case_study_attribute_score_data.Rds"))
 
 
-# Build data for typology
+# Build data for kruskal test of sig. diff of score among attributes and importance among attributes
 ################################################################################
+
+###temporarily remove "-" from Cross-scale for easier analysis later on
 
 data_orig <- data.frame(lapply(data_orig, function(x) {
   gsub("Cross-scale integration", "Crossscale integration", x)
@@ -34,17 +36,19 @@ data <- data_orig %>%
 
 
 
+################## Kruskal test of difference in score among attributes
+
 kruskal.test(score ~ attribute,
              data = data)
 
-
+################### post-doc Dunn Test with bh correction method 
 DT_score = dunnTest(score ~ attribute,
               data=data,
               method="bh")      
 
 
+############### df with only significant adjusted p-values
 PT = DT_score$res
-PT
 score_pairs<- PT%>% 
   filter(P.adj < .05) %>% arrange(P.adj)
 
@@ -52,55 +56,73 @@ score_pairs<- PT%>%
 library(tidyr)
 library(reshape2)
   
-  
+
+############################ break up comparison column 
 df_significant_score_difference<-  score_pairs %>% separate_wider_delim(Comparison, "-", names = c("attribute_1", "attribute_2"))
 
 df_significant_score_difference<- as.data.frame(df_significant_score_difference)
 
-
+######keep only adjusted p-values and attributes being compared
 df_diff_score<- df_significant_score_difference %>% select(attribute_1, attribute_2, P.adj)
 
-#df_diff_score_m <-acast(df_significant_score_difference, attribute_1~attribute_2, value.var="P.adj")
 
-
-
+##########add back in "-" in cross-scale integration
 
 df_diff_score <- data.frame(lapply(df_diff_score, function(x) {
   gsub("Crossscale integration", "Cross-scale integration", x)
 }))
 
-df_diff_score$P.adj<- as.numeric( df_diff_score$P.adj)
 
-df_diff_score %>% 
+df_diff_score$P.adj<- as.numeric( df_diff_score$P.adj) #as numeric for P.adjusted
+
+
+my_theme <-  theme(axis.text=element_text(size=6),
+                   axis.title=element_text(size=7),
+                   legend.text=element_text(size=6),
+                   legend.title=element_text(size=7),
+                   strip.text=element_text(size=7),
+                   # Gridlines
+                   panel.grid.major = element_blank(), 
+                   panel.grid.minor = element_blank(),
+                   panel.background = element_blank(), 
+                   axis.line = element_line(colour = "black"),
+                   # Legend
+                   legend.position="bottom",
+                   legend.background = element_rect(fill=alpha('blue', 0)))
+
+##########create heat map of p-values for significant pair-wise comparisons of score by attribute where p adjusted < 0.05
+pairwise_comparison_of_scores<- df_diff_score %>% 
 ggplot(aes(x=attribute_2, y=attribute_1, fill=P.adj)) + 
   geom_raster() +
-  scale_fill_viridis_c()+theme(axis.text.x = element_text(angle = 90))+
+  scale_fill_viridis_c(option = "G")+theme(axis.text.x = element_text(angle = 90))+
   theme(
     axis.title.x = element_blank(),
     axis.title.y = element_blank(),
-    panel.background = element_blank())
- 
-
-#df_diff_score<- as.data.frame(df_diff_score)
+    panel.background = element_blank()) +guides(fill=guide_legend(title="adjusted p-value")) +my_theme+ theme(legend.position = "none")
 
 
 
 
-##############################3-----------
+
+################## Kruskal test of difference in importance among attributes
 kruskal.test(importance_numeric~ attribute,
              data = data)
 
+
+################### post-doc Dunn Test with bh correction method 
 DT_importance = dunnTest(importance_numeric ~ attribute,
                     data=data,
                     method="bh")      
 
 
+############### df with only significant adjusted p-values
 PT_imp = DT_importance$res
 PT_imp
 pairs_imp<- PT_imp%>% 
   filter(P.adj < .05) %>% arrange(P.adj)
 
 
+############################ break up comparison column 
 df_significant_imp_difference<-  pairs_imp %>% separate_wider_delim(Comparison, "-", names = c("attribute_1", "attribute_2"))
 
 df_significant_imp_difference<- as.data.frame(df_significant_imp_difference)
@@ -108,25 +130,40 @@ df_significant_imp_difference<- as.data.frame(df_significant_imp_difference)
 
 df_diff_imp<- df_significant_imp_difference %>% select(attribute_1, attribute_2, P.adj)
 
-#df_diff_imp_m <-acast(df_significant_imp_difference, attribute_1~attribute_2, value.var="P.adj")
 
-
+##########add back in "-" in cross-scale integration
 
 
 df_diff_imp <- data.frame(lapply(df_diff_imp, function(x) {
   gsub("Crossscale integration", "Cross-scale integration", x)
 }))
 
-df_diff_imp$P.adj<- as.numeric(df_diff_imp$P.adj)
+df_diff_imp$P.adj<- as.numeric(df_diff_imp$P.adj) #p-value as numeric 
 
-df_diff_imp %>% 
+
+##########create heat map of p-values for significant pair-wise comparisons of importance by attribute where p adjusted < 0.05
+
+pairwise_comparison_of_imp<- df_diff_imp %>% 
   ggplot(aes(x=attribute_2, y=attribute_1, fill=P.adj)) + 
   geom_raster() +
-  scale_fill_viridis_c()+theme(axis.text.x = element_text(angle = 90))+
+  scale_fill_viridis_c(option = "G")+theme(axis.text.x = element_text(angle = 90))+
   theme(
     axis.title.x = element_blank(),
     axis.title.y = element_blank(),
-    panel.background = element_blank())
+    panel.background = element_blank()) +guides(fill=guide_legend(title="adjusted p-value")) +my_theme
+
+
+
+library(cowplot)
+
+
+supp_fig_comparisons<- plot_grid( pairwise_comparison_of_imp, pairwise_comparison_of_scores, labels = c('A', 'B'))
+
+
+
+ggsave(supp_fig_comparisons, filename=file.path(plotdir, "Final_supp_fig_comparisons.png"), 
+       width=7, height=4, units="in", dpi=600)
+
 
 
 
